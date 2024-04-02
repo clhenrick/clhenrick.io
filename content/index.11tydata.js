e362ee22445b5ed8193e09e302ba5a9542c7ea5d
Chris Henrick <chrishenrick@gmail.com>
2024-04-01 21:24:49 -0700

    use directoroy data for home page cards

---
+++ content/index.11tydata.js
@@ -0,0 +1,25 @@
+module.exports = {
+  services: [
+    {
+      title: "My Work",
+      description:
+        "Encompasses: Data Visualization, Web Accessibility, Prototyping, Interactive Web Maps, Interaction Design, and more.",
+      url: "/work/",
+      cta: "View Portfolio",
+    },
+    {
+      title: "Blog",
+      description:
+        "My ramblings on previous projects and solving problems relating to the web, data, cartography, and design.",
+      url: "/blog/",
+      cta: "View Blog",
+    },
+    {
+      title: "Talk to Me",
+      description:
+        "Want advice on how to get creative with your data? Need a custom mapping solution? Or just want to say hello?",
+      url: "/contact/",
+      cta: "Get in Touch",
+    },
+  ],
+};
