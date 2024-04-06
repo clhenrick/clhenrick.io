0d542b6fca1bc03637414861f08ad75c647ae673
Chris Henrick <161748+clhenrick@users.noreply.github.com>
2024-04-06 10:25:04 -0700

    Add content for more pages (#3)
    * added about page
    
    * added netlify status bagdge to readme
    
    * added content to about/cv/index
    
    * added content to about/about/index
    
    * added code comment to about/index
    
    * use card component layout & css in homepage
    
    * made project card a macro
    
    * use directoroy data for home page cards
    
    * added logging filter to 11ty config
    
    * renamed card.css -> project-card.css
    
    * updated code comment in project-card.css
    
    * move role="list" style overrides to reset.css
    
    * formatted code in base.njk
    
    * updated home cards styling
    
    * more home page cards styling
    
    * added missing role=list
    
    * minor homepage updates
    
    * added back small border radius for buttons
    
    * fixed font-weight in project cards
    
    * added contact form to contact/index
    
    * use base layout for contact page
    
    * added search page
    
    * primary navigation style updates
    
    * rm log filter from config
    
    * use css var for project card text margins

---
+++ content/index.11tydata.js
@@ -0,0 +1,25 @@
+module.exports = {
+  services: [
+    {
+      title: "Previous Projects",
+      description:
+        "Encompasses: Data Visualization, Web Accessibility, Prototyping, Interactive Web Maps, Interaction Design, and more.",
+      url: "/work/",
+      cta: "View Portfolio",
+    },
+    {
+      title: "Blog Articles",
+      description:
+        "Ramblings on previous projects and solving problems relating to the web, data, cartography, and design.",
+      url: "/blog/",
+      cta: "View Blog",
+    },
+    {
+      title: "A Conversation",
+      description:
+        "Want advice on how to get creative with your data? Need a custom mapping solution? Or just want to say hello?",
+      url: "/contact/",
+      cta: "Get in Touch",
+    },
+  ],
+};
