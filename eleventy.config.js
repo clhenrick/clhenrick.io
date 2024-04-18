515e7519aabd73d15aca6c16decdcdf45194b8ba
Chris Henrick <chrishenrick@gmail.com>
2024-04-18 08:01:13 -0700

    updated readableDate filter

--- eleventy.config.js
+++ eleventy.config.js
@@ -35,7 +35,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("readableDate", (dateObj, format, zone) => {
     // Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
     return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(
-      format || "dd LLLL yyyy"
+      format || "LLLL dd, yyyy"
     );
   });
 
