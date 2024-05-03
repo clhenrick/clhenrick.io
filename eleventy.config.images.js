b319ee6951c61e4c34bf0ea2bcc845a7f471bc0a
Chris Henrick <chrishenrick@gmail.com>
2024-05-02 18:41:25 -0700

    updated image shortcode config

--- eleventy.config.images.js
+++ eleventy.config.images.js
@@ -6,16 +6,21 @@ module.exports = function (eleventyConfig) {
   // https://www.11ty.dev/docs/plugins/image/
   eleventyConfig.addAsyncShortcode(
     "image",
-    async function imageShortcode(src, alt, widths, sizes) {
-      const formats = ["webp", "jpeg", "png", "gif", "svg"];
-
+    async function imageShortcode(
+      src,
+      alt = "",
+      widths = [960, 420, 1280],
+      sizes = "100vw"
+    ) {
+      const formats = ["webp", "jpeg", "svg"];
       const filename = src.split("/").pop();
-      const input = path.join(__dirname, "public", "img", filename );
+      const input = path.join(__dirname, "public", "img", filename);
 
       const metadata = await eleventyImage(input, {
-        widths: widths || ["auto"],
-        formats,
+        widths: [...widths, null], // null means the original size
+        formats: [...formats, null], // null means the original format
         outputDir: path.join(eleventyConfig.dir.output, "img"), // Advanced usage note: `eleventyConfig.dir` works here because we’re using addPlugin.
+        outputPath: "/img/",
       });
 
       // TODO loading=eager and fetchpriority=high
