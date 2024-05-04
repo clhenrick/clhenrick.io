0579ec3436fe7d147ad85b581f55ecd45265cc65
Chris Henrick <chrishenrick@gmail.com>
2024-05-04 09:09:53 -0700

    added script for find and replace of markdown img syntax with 11ty image shortcode

---
+++ scripts/find-and-replace-md-img-tags.mjs
@@ -0,0 +1,44 @@
+#!/usr/bin/env zx
+import 'zx/globals';
+
+/**
+ * find-and-replace-md-img-tags.mjs
+ * Node.js ESM script that will find and replace markdown image syntax with an Eleventy Nunjucks 'image' shortcode.
+ * Note: this script uses the zx library: https://google.github.io/zx/
+ * Warning: this script will write to the input file!
+ * Usage: find-and-replace-md-img-tag.mjs --file=/path/to/file.md
+ */
+
+const input = argv.file;
+if (!input) {
+  echo('No input file specified');
+  echo('usage: find-and-replace-md-img-tag.mjs --file=/path/to/file.md');
+  process.exit(0);
+}
+echo('Processing file: ' + input);
+
+const data = fs.readFileSync(input, { encoding: 'utf8' });
+
+const regex = /\!\[(.*?)\]\((.*)\)$/gm;
+
+const matches = data.matchAll(regex);
+const matchesArray = Array.from(matches);
+
+if (!matchesArray.length) {
+  echo('No instances of ![]() found, exiting.');
+  process.exit(0);
+}
+
+let processed = data;
+
+matchesArray.forEach(([match, altText, imgPath]) => {
+  const fileName = imgPath.split('/').pop();
+  const imgShortCode = `{% image '${fileName}', '${altText || ''}' %}`;
+  processed = processed.replace(match, imgShortCode);
+});
+
+echo('Writing file: ' + input);
+fs.writeFileSync(input, processed, { encoding: 'utf8' });
+echo('Done.');
+
+process.exit(0);
