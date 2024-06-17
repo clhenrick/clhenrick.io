a631c10d17091a3863c04c6aee337b14352dc9d4
Chris Henrick <chrishenrick@gmail.com>
2024-06-17 07:44:02 -0700

    updated code comments

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,5 +1,5 @@
 // Progressively enhanced search form for /search/index.njk
-// This still works without JavaScript; here we remove the prepopulated "site:clhenrick.io" from the text input's value so that it doesn't distract the user when they enter a search query.
+// NOTE: the search form still works without this JavaScript; here we just remove the pre-populated "site:clhenrick.io" from the text input's value so that it doesn't distract the user when they enter a search query.
 (() => {
   const form = document.querySelector('form[role="search"]');
   const textInput = document.querySelector('input[type="text"]');
