9db9a0fc07dfb37a422a1de053302f4e6f7036fb
Chris Henrick <161748+clhenrick@users.noreply.github.com>
2024-07-09 18:00:17 -0700

    Footer styles updates (#43)
    * updated footer styles:
    - fix spacing of sections on wide page layouts
    - rm bottom margin of link lists
    
    * updated global metadata:
    - added github repo property
    
    * updated footer: use metadata github repo for code url
    
    * updated global metadata:
    - added author url
    
    * simplified footer styles:
    
    - rm breakpoints
    - rm height & width restrictions on .social-links
    - adjust margins and spacings
    
    * updated global metadata:
    - added social links map
    
    * updated footer: use metadata.social to create list
    
    * rm target="_blank" from links
    
    * cleaned up formatting of html / nunjucks code
    
    * only use html space code in visually-hidden span
    
    * added outline property to links for forced-colors mode

--- _data/metadata.js
+++ _data/metadata.js
@@ -4,9 +4,18 @@ module.exports = {
   language: "en",
   description: "Web developer and design engineer specializing in data visualization, web accessibility, and interactive geographic mapping.",
   environment: process.env.NODE_ENV,
+  githubRepository: "https://github.com/clhenrick/portfolio-new",
   author: {
     name: "Chris Henrick",
     email: "chrishenrick@gmail.com",
-    url: "",
+    url: "https://clhenrick.io/about/",
   },
+  social: new Map([
+    ["GitHub", "https://github.com/clhenrick"],
+    ["Mastodon", "https://indieweb.social/@clhenrick"],
+    ["X (Twitter)", "https://twitter.com/chrislhenrick"],
+    ["Observable", "https://observablehq.com/@clhenrick"],
+    ["LinkedIn", "https://www.linkedin.com/in/chrishenrick/"],
+    ["Ko-Fi", "https://ko-fi.com/chrislhenrick"]
+  ]),
 };
