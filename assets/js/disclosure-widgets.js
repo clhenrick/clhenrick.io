f397554f9c7e7c21140c5e20188124dbf2bbc91c
Chris Henrick <chrishenrick@gmail.com>
2024-07-30 08:57:35 -0700

    adjust breakpoint for collapsing disclosures

--- assets/js/disclosure-widgets.js
+++ assets/js/disclosure-widgets.js
@@ -1,6 +1,7 @@
 (() => {
   const disclosures = document.querySelectorAll("details");
-  const mediaQueryNarrowViewports = window.matchMedia("(max-width: 400px)");
+  // NOTE: 500px is about when the filter buttons wrap to three lines and pushes the project cards further down the page
+  const mediaQueryNarrowViewports = window.matchMedia("(max-width: 500px)");
   // collapse disclosure elements on narrow viewports
   if (mediaQueryNarrowViewports.matches && disclosures.length) {
     disclosures.forEach((disclosure) => {
