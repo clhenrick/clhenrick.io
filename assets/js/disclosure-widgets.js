4af7d60aea16837393df669421a725de6f1b36e4
Chris Henrick <chrishenrick@gmail.com>
2024-07-30 09:17:42 -0700

    update disclosure widget collapse breakpoint

--- assets/js/disclosure-widgets.js
+++ assets/js/disclosure-widgets.js
@@ -1,7 +1,7 @@
 (() => {
   const disclosures = document.querySelectorAll("details");
-  // NOTE: 500px is about when the filter buttons wrap to three lines and pushes the project cards further down the page
-  const mediaQueryNarrowViewports = window.matchMedia("(max-width: 500px)");
+  // NOTE: matches media query breakpoint in primary-navigation
+  const mediaQueryNarrowViewports = window.matchMedia("(max-width: 747px");
   // collapse disclosure elements on narrow viewports
   if (mediaQueryNarrowViewports.matches && disclosures.length) {
     disclosures.forEach((disclosure) => {
