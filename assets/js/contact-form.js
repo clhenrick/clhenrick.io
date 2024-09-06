e3ae26003766b258347eff2347cc873d516cd180
Chris Henrick <chrishenrick@gmail.com>
2024-09-05 20:20:53 -0700

    stub out basic form validation error handling when inputs are empty

--- assets/js/contact-form.js
+++ assets/js/contact-form.js
@@ -16,11 +16,66 @@
   /** @type { HTMLTextAreaElement} */
   const textAreaMessage = document.querySelector("#message");
 
+  /** @type { HTMLParagraphElement } */
+  const errorMsgName = document.querySelector("#full-name-error-msg");
+
+  /** @type { HTMLParagraphElement} */
+  const errorMsgEmail = document.querySelector("#email-address-error-msg");
+
+  /** @type { HTMLParagraphElement } */
+  const errorMsgNote = document.querySelector("#note-error-msg");
+
   // prefer `aria-required` over `required` for enhanced accessibility and custom form validation
   [inputName, inputEmail, textAreaMessage].forEach((element) => {
     element.removeAttribute("required");
     element.setAttribute("aria-required", true);
+    element.setAttribute("aria-invalid", "false");
   });
+
+  form.addEventListener("submit", handleFormSubmit);
+
+  /**
+   * @param {FormData} data
+   * @returns {boolean}
+   * */
+  function validateForm(data) {
+    let isValid = true;
+
+    if (!data.name) {
+      // require name
+      isValid = false;
+      errorMsgName.innerText = "Please provide your name.";
+      errorMsgName.removeAttribute("hidden");
+      inputName.setAttribute(
+        "aria-describedby",
+        errorMsgName.getAttribute("id")
+      );
+    }
+
+    if (!data.message) {
+      // require message
+      isValid = false;
+      errorMsgNote.innerText = "Please tell me what you're writing me about.";
+      errorMsgNote.removeAttribute("hidden");
+      textAreaMessage.setAttribute(
+        "aria-describedby",
+        errorMsgNote.getAttribute("id")
+      );
+    }
+
+    if (!data["_replyto"]) {
+      // require email
+      isValid = false;
+      errorMsgEmail.innerText = "Please provide a valid email address.";
+      errorMsgEmail.removeAttribute("hidden");
+      inputEmail.setAttribute(
+        "aria-describedby",
+        errorMsgEmail.getAttribute("id")
+      );
+    }
+
+    return isValid;
+  }
 
   /** @param {SubmitEvent} event */
   function handleFormSubmit(event) {
@@ -28,6 +83,14 @@
     console.log("submit!");
 
     const data = new FormData(event.target);
+    console.log(data);
+
+    const isValid = validateForm(data);
+    console.log(isValid);
+
+    if (!isValid) {
+      return;
+    }
 
     fetch(event.target.action, {
       method: form.method,
@@ -54,6 +117,4 @@
         // TODO: move focus to #form-status
       });
   }
-
-  form.addEventListener("submit", handleFormSubmit);
 })();
