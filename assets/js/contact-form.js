83fbf0e5c61c48f9de8d5a21889405b1a861c7a8
Chris Henrick <chrishenrick@gmail.com>
2024-11-17 18:33:38 -0800

    updated contact form status message:
    - update status message when a form field changes from invalid to valid
    - remove status error message when all form fields are valid before submitting

--- assets/js/contact-form.js
+++ assets/js/contact-form.js
@@ -128,9 +128,10 @@
 
   /**
    * - handles a client or server side form validation error
-   * @param {(HTMLInputElement | HTMLTextAreaElement)[] | string} [errors] either a string of error text that should be shown or an array of form fields that are in an invalid state.
+   * @param {(HTMLInputElement | HTMLTextAreaElement)[] | string} errors either a string of error text that should be shown or an array of form fields that are in an invalid state.
+   * @param {boolean} shouldFocus whether to focus the form status field after updating its text content; defaults to true
    */
-  function handleInvalidFormState(errors) {
+  function handleInvalidFormState(errors, shouldFocus = true) {
     // default error message
     let errorMessage =
       "Something went wrong when submitting the form. Please try submitting the form again or alternatively you may send me an email. Thanks!";
@@ -149,11 +150,22 @@
       errorMessage = `Please correct the error${pluralHandler} in the ${fieldTypes} form field${pluralHandler}.`;
     }
 
+    // if an empty array is passed from a result of no [aria-invalid="true"], truncate the errorMessage as to reset the form status to empty
+    if (!errors?.length) {
+      errorMessage = "";
+    }
+
     // for any error situation
-    formStatus.innerText = errorMessage;
-    formStatus.classList = "error";
-    formStatus.removeAttribute("hidden");
-    formStatus.focus();
+    if (errorMessage) {
+      formStatus.innerText = errorMessage;
+      formStatus.classList = "error";
+      formStatus.removeAttribute("hidden");
+      shouldFocus && formStatus.focus();
+    } else {
+      formStatus.innerText = "";
+      formStatus.classList = "";
+      formStatus.setAttribute("hidden", "");
+    }
   }
 
   function handleFormSubmitSuccess() {
@@ -266,5 +278,9 @@
       // since disabled buttons aren't the best for a11y (poor color contrast,
       // removed from tab order, etc).
     }
+
+    // update the form status message
+    const invalidFormFields = getInvalidFormFields();
+    handleInvalidFormState(Array.from(invalidFormFields), false);
   }
 })();
