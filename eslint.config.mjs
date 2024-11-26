8028beff0c5745dbe2d57c3eef399ffa83793b2a
Chris Henrick <chrishenrick@gmail.com>
2024-11-26 10:03:17 -0800

    updated eslint config to play nicely with prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
+  eslintConfigPrettier,
 ];
