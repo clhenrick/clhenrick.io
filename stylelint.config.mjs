52996a966c14198c2aa449a4f5bb284453dafd06
Chris Henrick <chrishenrick@gmail.com>
2024-11-27 17:23:59 -0800

    updated stylelint config

---
+++ stylelint.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: ["stylelint-config-standard", "stylelint-prettier/recommended"],
+  rules: {
+    "custom-property-empty-line-before": null,
+    "declaration-empty-line-before": null,
+    "value-keyword-case": null,
+  },
+};
